d712955123e6ed5506259800e42c2c9ddac97641
Jitendra Mishra <jitendra29mishra@gmail.com>
2025-09-21 10:58:24 +0530

    Update and rename hello.py to Cargo.toml

--- hello.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-
-
-api = FastAPI()
-
-@api.get("/")
-async def index():
-  return JSONResponse(content={"hello": "world!!"}, status_code=200)
